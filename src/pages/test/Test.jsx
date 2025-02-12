742b87c7d04edf2abd4c135486ffbe27243cb22c
Yourixf <Yourivanbeuzekom@upcmail.nl>
2025-02-13 00:50:52 +0100

    4829 pogingen gedaan, 403 error wint

--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -1,34 +1,26 @@
 // import React from 'react';
 // import './Recommendations.css';
 import axios from "axios";
-
 import Button from "../../components/button/Button.jsx";
 
 function Test () {
-    const noviBaseApiEndpoint = "https://api.datavortex.nl/gamecatalogue";
+    const randomJoke = "https://api.chucknorris.io/jokes/random";
+
+    const NOVI_BASE_API_ENDPOINT = "https://api.datavortex.nl/gamecatalogue";
+
     const noviBaseHeaders = {
         'Content-type': 'application/json',
-        'X-Api-Key':import.meta.env.VITE_NOVI_API_KEY
+        'X-Api-Key': `${import.meta.env.VITE_NOVI_API_KEY}`,
     };
-
-    const noviBaseHeaders2 = {
-        'Content-Type': 'application/json',
-        'X-Api-Key': 'gamecatalogue:QCNnRs9yfPCbZmu2BT3h'
-    }
-
 
     async function testAPI() {
         try {
-            const result = await axios.get(`${noviBaseApiEndpoint}/info`, {
-                'Content-Type': 'application/json',
-                'X-Api-Key': 'gamecatalogue:QCNnRs9yfPCbZmu2BT3h'
-            });
+            const result = await axios.get(`${NOVI_BASE_API_ENDPOINT}/info`, {headers:noviBaseHeaders});
             console.log(result)
         }
         catch (e) {
             console.log(e)
         }
-
     }
 
     return(
