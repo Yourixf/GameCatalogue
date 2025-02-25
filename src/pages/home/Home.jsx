2005e8614007f37a529056b16a552732ed7948a9
Yourixf <Yourivanbeuzekom@upcmail.nl>
2025-02-25 12:06:25 +0100

    bezig met de filter opties op de home pagina

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -27,47 +27,56 @@ function Home () {
 
             <section className={`section-outer-container recommended-section-outer`}>
                 <div className={`section-inner-container recommended-section-inner ${selectedTheme}`}>
-                    <p className={`section-title recommended-title`}>Aanbevolen voor jou</p>
-                    <div className={`recommended-card ${selectedTheme}`}>
-                        <figure className={`recommended-game-image-wrapper`}>
-                            <img className={`recommended-game-image`} src={pubgImg} alt="game-image"/>
 
-                        </figure>
+                     <span className={"sorting-filter-wrapper state-one"}>
+                       <SortingFilter content={"Sorteer op:"} type={'sorting'}/>
+                       <SortingFilter content={"Filter op:"} type={"filter"}/>
+                    </span>
 
-                        <span className={`recommended-game-info-wrapper`}>
-                            <div className={`recommended-text-info`}>
-                                <p className={`recommended-text-description`}>Titel</p>
-                                <p className={`recommended-text-content`}>Pubg</p>
-                            </div>
+                    <span className={"recommended-section-wrapper"}>
+                        <h2 className={`section-title recommended-title`}>Aanbevolen voor jou</h2>
+                        <article className={`recommended-card ${selectedTheme}`}>
+                            <figure className={`recommended-game-image-wrapper`}>
+                                <img className={`recommended-game-image`} src={pubgImg} alt="game-image"/>
 
-                            <div className={`recommended-text-info`}>
-                                <p className={`recommended-text-description`}>Uitgifte datum</p>
-                                <p className={`recommended-text-content`}>23 maart 2017</p>
-                            </div>
-                            <div className={`recommended-text-info`}>
-                                <p className={`recommended-text-description`}>Metascore</p>
-                                <p className={`recommended-text-content`}>81</p>
-                            </div>
-                            <figure className={"recommended-game-card-platforms"}>
-                                <img className={"platform-icon"} src={windwows} alt="windows-icon"/>
-                                <img className={"platform-icon"} src={playstation} alt="playstation-icon"/>
-                                <img className={"platform-icon"} src={xbox} alt="xbox-icon"/>
-                                <img className={"platform-icon"} src={apple} alt="apple-icon"/>
-                                <img className={"platform-icon"} src={android} alt="android-icon"/>
-                                <img className={"platform-icon"} src={nitendoswitch} alt="nitendoswitch-icon"/>
                             </figure>
-                        </span>
-                    </div>
-                    <div className={`recommended-pagination`}>
-                        <span className={`recommended-pagination-other currently-selected`}></span>
-                        <span className={`recommended-pagination-other`}></span>
-                        <span className={`recommended-pagination-other`}></span>
-                        <span className={`recommended-pagination-other`}></span>
-                        <span className={`recommended-pagination-other`}></span>
-                        <span className={`recommended-pagination-other`}></span>
+
+                            <span className={`recommended-game-info-wrapper`}>
+                                <div className={`recommended-text-info`}>
+                                    <h3 className={`recommended-text-description`}>Titel</h3>
+                                    <h3 className={`recommended-text-content`}>Pubg</h3>
+                                </div>
+
+                                <div className={`recommended-text-info`}>
+                                    <h3 className={`recommended-text-description`}>Uitgifte datum</h3>
+                                    <h3 className={`recommended-text-content`}>23 maart 2017</h3>
+                                </div>
+                                <div className={`recommended-text-info`}>
+                                    <h3 className={`recommended-text-description`}>Metascore</h3>
+                                    <h3 className={`recommended-text-content`}>81</h3>
+                                </div>
+                                <figure className={"recommended-game-card-platforms"}>
+                                    <img className={"platform-icon"} src={windwows} alt="windows-icon"/>
+                                    <img className={"platform-icon"} src={playstation} alt="playstation-icon"/>
+                                    <img className={"platform-icon"} src={xbox} alt="xbox-icon"/>
+                                    <img className={"platform-icon"} src={apple} alt="apple-icon"/>
+                                    <img className={"platform-icon"} src={android} alt="android-icon"/>
+                                    <img className={"platform-icon"} src={nitendoswitch} alt="nitendoswitch-icon"/>
+                                </figure>
+                            </span>
+                        </article>
+                        <nav className={`recommended-pagination`} aria-label={"recommended games pagination"}>
+                            <span className={`recommended-pagination-other currently-selected`}
+                                  aria-label={"game-1"}></span>
+                            <span className={`recommended-pagination-other`} aria-label={"game 2"}></span>
+                            <span className={`recommended-pagination-other`} aria-label={"game 3"}></span>
+                            <span className={`recommended-pagination-other`} aria-label={"game 4"}></span>
+                            <span className={`recommended-pagination-other`} aria-label={"game 5"}></span>
+                            <span className={`recommended-pagination-other`} aria-label={"game 6"}></span>
 
 
-                    </div>
+                        </nav>
+                    </span>
                 </div>
 
             </section>
@@ -76,11 +85,16 @@ function Home () {
             <section className={`section-outer-container trending-games`}>
                 <div className={`section-inner-container trending-games-section-inner`}>
                     <div>
-                        <p className={`section-title`}>
+                        <h2 className={`section-title`}>
                             Uitgelicht
-                        </p>
+                        </h2>
 
                     </div>
+                    <span className={"sorting-filter-wrapper state-two"}>
+                       <SortingFilter content={"Sorteer op:"} type={'sorting'}/>
+                       <SortingFilter content={"Filter op:"} type={"filter"}/>
+                    </span>
+
                     <div className={`game-card-wrapper`}>
                         <GameCard/>
                         <GameCard/>
@@ -95,7 +109,7 @@ function Home () {
                         <GameCard/>
 
                     </div>
-
+                    <Pagination/>
                 </div>
             </section>
 
