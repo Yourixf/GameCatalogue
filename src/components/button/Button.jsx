8745a0f9630174ad6e07a0b311df6626dd4ec919
Yourixf <Yourivanbeuzekom@upcmail.nl>
2025-04-19 01:10:53 +0200

    fix search query bij geen query, sorteer functie nog niet werkend op rec pagina, button tekst kleur ingesteld voor mobiele telefoons

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,6 +1,7 @@
 import "./Button.css";
 
 function Button ({onClick, disabled=false, className="", content="", type="button", shadow=true, children}) {
+
     return (
         <button type={type} onClick={onClick} disabled={disabled} className={"button-main " + [shadow?"button-shadow ": ""] + className}>
             <span className={"button-content"}> {content}</span>
